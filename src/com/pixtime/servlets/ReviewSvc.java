529c5eb74a6d7bd99886e6c606acf359348c5f9b
Aasi-solutions <aasisolutions@gmail.com>
2014-01-09 13:34:12 +0530

    update dopost method to call insertReview()
    by dhanalakshmi

--- src/com/pixtime/servlets/ReviewSvc.java
+++ src/com/pixtime/servlets/ReviewSvc.java
@@ -30,25 +30,38 @@ public class ReviewSvc extends HttpServlet {
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+
 		// TODO Auto-generated method stub
 		String pixtimeStatusDesc = "Review Added";
 		String pixtimeStatus = "SUCCESS";
 		Gson gson = new Gson();
 
 		try {
-
 			Reader reader = new InputStreamReader(req.getInputStream());
-
-			Review movie = gson.fromJson(reader, Review.class);
-			
+			String title=req.getParameter("fc");
+			String movieid=req.getParameter("movieid");
+			String taip=req.getParameter("ta");
+			String userid=req.getParameter("t1");
+			System.out.println("title  taip  "+title+"  "+taip+"     "+userid);
+	                    System.out.println("movieid res"+ movieid.trim());
+			Review review = gson.fromJson(reader, Review.class);
+		
+		String comment=title+" "+taip;
+		
+		
+			ReviewDao rvDao = new ReviewDao();
+			rvDao.insertReview(movieid,comment,userid);
+			resp.setStatus(201);
 			
 		}catch(Exception e){
 			e.printStackTrace();
 			pixtimeStatusDesc = "internal Error Occured. Please check with us Later";
 			pixtimeStatus = "FAILED";
 			resp.setStatus(500);
+			LOG.info("Review Add Exception Occured" + e.toString());
+
 		
 		}
 		resp.setContentType(PixTimeConstants.CONTENT_TYPE);
